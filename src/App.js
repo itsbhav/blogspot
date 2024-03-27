2f5855be36c785379d20bee26e96e12bab57f453
itsbhav <118657839+itsbhav@users.noreply.github.com>
2024-03-27 20:55:21 +0530

    route update

--- src/App.js
+++ src/App.js
@@ -9,35 +9,35 @@ import Public from "./components/Public";
 import PostPagePrivate from "./components/PostPagePrivate";
 import PersistLogin from "./features/auth/PersistLogin";
 import Prefetch from "./features/auth/Prefetch";
-import NewPost from "./components/NewPost"
+import NewPost from "./components/NewPost";
 import EditUser from "./components/EditUser";
-import VerifyMail from './components/VerifyMail'
+import VerifyMail from "./components/VerifyMail";
 import ForgotPass from "./components/ForgotPass";
 function App() {
-  return ( 
+  return (
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* public  */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="recoverPassword" element={<ForgotPass />} />
         <Route element={<PersistLogin />}>
           <Route element={<Prefetch />}>
             {/* Protected */}
             <Route path="dash" element={<DashBoard />} />
             <Route path="posts">
-              <Route index element={<DashBoard/>}/>
+              <Route index element={<DashBoard />} />
               <Route path="new" element={<NewPost />} />
-              <Route path=":id" element={<PostPagePrivate/>} />
+              <Route path=":id" element={<PostPagePrivate />} />
             </Route>
             <Route path="users">
               <Route index element={<UserList />} />
-              <Route path="edit" element={<EditUser/>}/>
+              <Route path="edit" element={<EditUser />} />
               <Route path=":id" element={<UserPage />} />
             </Route>
           </Route>
           <Route path="verificationPage" element={<VerifyMail />} />
-          <Route path="recoverPassword" element={<ForgotPass/>}/>
         </Route>
       </Route>
     </Routes>
